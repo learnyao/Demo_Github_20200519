5a7be6c5f25bbd62553b36aa1a448b5a7d65cc47
learnyao <64775236+learnyao@users.noreply.github.com>
2020-05-18 16:32:04 +0800

    yao
    #123456

--- Demo_Upload.py
+++ Demo_Upload.py
@@ -12,6 +12,8 @@ os.chdir(r'filepath') #將當前路徑轉至圖片所在的資料夾
 
 outputpath = 'outputfilepath' #合併後的圖要放的路徑以及格式  如：C:\\Users\\username\\output.png
 
+#123
+#456
 
 img_1 = io.imread('1st_img_name') #要合併的上（左）圖
 img_2 = io.imread('2nd_img_name') #要合併的下（右）圖
